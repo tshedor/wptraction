fb40a9e6680927d42831e2b9a7d6d3a180c73930
Tim Shedor <tshedor@gmail.com>
2013-05-06 19:51:58 -0500

    more magnific fixes

--- ts-stds/libs/magnific/magnific-custom.js
+++ ts-stds/libs/magnific/magnific-custom.js
@@ -1,29 +1,28 @@
 jQuery(document).ready(function ($) {
-    $(".gallery").magnificPopup({
-        mainClass: "mfp-zoom-in",
-        removalDelay: 300,
-        delegate: "a",
-        type: "image",
-        tLoading: "Loading image #%curr%...",
-        gallery: {
-            enabled:true,
-            navigateByImgClick:true,
-            preload: [0, 1]
-        },
-        image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-            titleSrc: function (e) {
-                e.el.attr("alt")
-            }
-        }
-    });
-    $('a[href*=".jpg"], a[href*=".png"], a[href*=".jpeg"]').magnificPopup({
-        type: "image",
-        closeOnContentClick:true,
-        mainClass: "mfp-zoom-in",
-        removalDelay: 300,
-        image: {
-            verrticalFit:true
-        }
-    })
+	$(".gallery").magnificPopup({
+		mainClass: "mfp-zoom-in",
+		removalDelay: 300,
+		delegate: "a",
+		type: "image",
+		tLoading: "Loading image #%curr%...",
+		gallery: {
+			enabled:true,
+			navigateByImgClick:true,
+			preload: [0, 1]
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: 'alt'
+		}
+	});
+	$('p a[href*=".jpg"], p a[href*=".png"], p a[href*=".jpeg"]').magnificPopup({
+		type: "image",
+		closeOnContentClick:true,
+		mainClass: "mfp-zoom-in",
+		removalDelay: 300,
+		image: {
+			verticalFit:true,
+			titleSrc: 'alt'
+		}
+	})
 });
